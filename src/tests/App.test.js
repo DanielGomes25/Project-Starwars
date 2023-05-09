3fe77bbee689c2689f91c8cfa6d65937cb538bfa
Daniel <daniel.gomes.9@hotmail.com>
2023-05-09 11:34:11 -0300

    resolve requisito 8

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -62,30 +62,114 @@ it('testes para o componente filterNumber', async () => {
   
   </MyProvider>,
   )
-  const inputName = screen.getByTestId('name-filter')
   const getColumn = screen.getByTestId('column-filter')
   const getComparison = screen.getByTestId('comparison-filter')
   const getValuFilter = screen.getByTestId('value-filter')
   const getButtonFilter = screen.getByTestId('button-filter')
   const getButtonRemoveFilter = screen.getByRole('button', {
-    name: 'FILTRAR'
+    name: 'REMOVER FILTROS'
   })
 
   expect(await screen.findAllByTestId('planet-name')).toHaveLength(10)
   
-  userEvent.click(getButtonFilter)
+  
+  userEvent.clear(getValuFilter)
 
-userEvent.clear(inputName)
   userEvent.selectOptions(getColumn, 'diameter')
   userEvent.selectOptions(getComparison, 'igual a')
   userEvent.type(getValuFilter, '4900')
 
+  userEvent.click(getButtonFilter)
+  expect(screen.getAllByTestId('planet-name')).toHaveLength(1)
+
+  const getRemoveFilter = screen.getByRole('button', {
+    name: 'Remover Filtro'
+  })
+  userEvent.click(getRemoveFilter)
+
+  expect(getButtonFilter).toBeInTheDocument()
+
   act(() => {
 
-    userEvent.click(getButtonFilter)
+    userEvent.click(getButtonRemoveFilter)
   })
-  
-expect(screen.getAllByTestId('planet-name')).toHaveLength(1)
-})
+  expect(screen.getAllByTestId('planet-name')).toHaveLength(10)
 
+})
+it('testes para o componente filter number verificar o filtro de ascendente e descendente', async () => {
+  render(
+    <MyProvider>
+    <App />
+  
+  </MyProvider>,
+  )
+  const getColumn = screen.getByTestId('column-filter')
+  const getComparison = screen.getByTestId('comparison-filter')
+  const getValuFilter = screen.getByTestId('value-filter')
+  const getButtonFilter = screen.getByTestId('button-filter')
+  const getButtonRemoveFilter = screen.getByRole('button', {
+    name: 'REMOVER FILTROS'
+  })
+  const getColumnSort =  screen.getByTestId('column-sort')
+const getSortInputAsc = screen.getByTestId('column-sort-input-asc')
+const getSortInputDes = screen.getByTestId('column-sort-input-desc')
+const getButtonSort = screen.getByTestId('column-sort-button')
+
+userEvent.selectOptions(getColumnSort, 'population')
+userEvent.click(getSortInputAsc)
+userEvent.click(getButtonSort)
+
+expect(await screen.findByRole('cell', {
+  name: /Yavin IV/i
+})).toBeInTheDocument()
+
+userEvent.selectOptions(getColumnSort, 'population')
+userEvent.click(getSortInputDes)
+userEvent.click(getButtonSort)
+
+expect(await screen.findByRole('cell', {
+  name: /Coruscant/i
+})).toBeInTheDocument()
+
+userEvent.click(getButtonRemoveFilter)
+
+expect(screen.getAllByTestId('planet-name')).toHaveLength(10)
+
+userEvent.clear(getValuFilter)
+
+userEvent.selectOptions(getColumn, 'surface_water')
+  userEvent.selectOptions(getComparison, 'menor que')
+  userEvent.type(getValuFilter, '40')
+
+  userEvent.click(getButtonFilter)
+  userEvent.selectOptions(getColumnSort, 'population')
+userEvent.click(getSortInputAsc)
+
+userEvent.click(getButtonSort)
+
+expect(await screen.findByRole('cell', {
+  name: /Bespin/i
+})).toBeInTheDocument()
+
+userEvent.click(getButtonRemoveFilter)
+
+expect(screen.getAllByTestId('planet-name')).toHaveLength(10)
+
+userEvent.clear(getValuFilter)
+
+userEvent.selectOptions(getColumn, 'rotation_period')
+  userEvent.selectOptions(getComparison, 'maior que')
+  userEvent.type(getValuFilter, '24')
+
+  userEvent.click(getButtonFilter)
+  userEvent.selectOptions(getColumnSort, 'population')
+userEvent.click(getSortInputDes)
+
+userEvent.click(getButtonSort)
+
+expect(await screen.findByRole('cell', {
+  name: /Kamino/i
+})).toBeInTheDocument()
+
+  })
 })
